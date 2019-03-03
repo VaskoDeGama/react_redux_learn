d6031cf63195cd666a4af2a3ba75a6f4b17e5eb0
VaskoDeGama <Vasr161095@gmail.com>
2019-03-03 21:38:55 +0300

    add custom css

--- todo/src/components/todo-list.js
+++ todo/src/components/todo-list.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import TodoListItem from './todo-list-item';
+import './todo-list.css';
 
 const TodoList = ({ todos }) => {
     
     const elements = todos.map((item) => {
         const {id, ...itemProps} = item;
         return (
-            <li key = {id}>
+            <li key = {id} className="list-group-item">
                 <TodoListItem  {  ...itemProps}  />
             </li>
         )
@@ -14,7 +15,7 @@ const TodoList = ({ todos }) => {
 
 
     return (
-        <ul>
+        <ul className="list-group todo-list">
             {elements}
         </ul>
     );
