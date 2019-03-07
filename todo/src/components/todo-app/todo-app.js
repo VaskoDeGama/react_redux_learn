351db9be872c806319c99fe84bbc46e2290c4310
VaskoDeGama <Vasr161095@gmail.com>
2019-03-07 06:49:45 +0500

    some work with state !dont chenge back state!

--- todo/src/components/todo-app/todo-app.js
+++ todo/src/components/todo-app/todo-app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import TodoHeader from '../todo-header';
 import SearchPanel from '../todo-searchpanel';
 import TodoList from '../todo-list';
@@ -6,28 +6,46 @@ import StatusFilter from '../item-status-filter'
 
 import './todo-app.css'
 
-const TodoApp= () => {
+export default class TodoApp extends Component {
+    state = {
+        todoData : [
+            {label: 'Drink coffee', id: 123},
+            {label: 'Make awesome app', id: 124},
+            {label: 'Have a lunch', id: 125},
 
-    const todoData = [
-        {label: 'Drink coffee', important: false, id: 123},
-        {label: 'Make awesome app', important: true, id: 124},
-        {label: 'Have a lunch', important: false, id: 125},
-        
-    ]
+        ]
+    };
 
-    return (
-        <div className="todo-app">
-            <TodoHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <StatusFilter />
+    deleteItem = (id) => {
+        this.setState(({todoData}) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+            const newArr = [
+                ...todoData.slice(0,idx),
+                ...todoData.slice(idx+1)
+            ];
+            return {
+                todoData: newArr
+            }
+        });
+    };
+
+
+    render() {
+        const {todoData} = this.state;
+        console.log(todoData);
+        return (
+            <div className="todo-app">
+                <TodoHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <StatusFilter />
+                </div>
+
+                <TodoList todos = { todoData  } onDeleted = {this.deleteItem} />
             </div>
-            
-            <TodoList todos = { todoData }  />
-        </div>
-    );
-    
+        );
+    }
+}
 
-};
 
-export default TodoApp;
+
