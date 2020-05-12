5124093d85d92766ff0400c9f1e5ee996854cdfb
VaskoDeGama <vasr161095@gmail.com>
2020-05-12 13:45:12 +0800

    useContext

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,18 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import ReactDOM from 'react-dom';
+
+const Context = React.createContext('Hello', undefined);
 
 const App = () => {
 
     return (
-        <div>
-            Hi
-        </div>
+        <Context.Provider value={'Hello world'} >
+            <Child />
+        </Context.Provider>
         )
-
 }
 
 
-
+const Child = () => {
+    const text = useContext(Context)
+    return (
+        <h2>{text}</h2>
+    )
+}
 
 ReactDOM.render(
     <App />,
